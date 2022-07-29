398b21d90b26b8ae04dc022a4ececd00342e6c71
Isaiah7579 <70917926+Isaiah7579@users.noreply.github.com>
2022-07-29 11:16:05 -0500

    updated document

--- definitions.py
+++ definitions.py
@@ -5,3 +5,4 @@ DEFAULT_CONFIG_PATH = os.path.join(ROOT_DIR, "Program_Data/default.yaml")  # req
 LOCAL_CONFIG_PATH = os.path.join(ROOT_DIR, "Program_Data/local.yaml")
 POWER_METER_DLL_PATH = os.path.join(ROOT_DIR, "Program_Data")
 SYSTEM_INFO_INI_PATH = os.path.join(ROOT_DIR, "Program_Data/systeminfo.ini")
+
